182fae8417122fd8256483d80d3248bbcf13cba8
MugdhaRahman <mugdharahman11@gmail.com>
2025-01-08 01:32:54 +0600

    motion animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+import { animate, scroll } from "motion"
 
 
 // Import all of Bootstrap's JS
@@ -9,3 +10,17 @@ import Alert from 'bootstrap/js/dist/alert'
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap'
+
+animate(
+    ".hero-img",
+    { rotate: 360 },
+    { type: "spring", repeat: Infinity, repeatDelay: 0.5 }
+)
+
+const animation = animate(
+    ".grey-bg",
+    { transform: ["none", "rotate(100deg)"] },
+    { ease: "linear" }
+)
+
+scroll(animation)
